0d08496f72458c589d59f9e6e05ca05ec5b09d3f
koby <kobyappiah23@gmail.com>
2025-09-11 21:11:34 +0000

    second commit

--- word_count.py
+++ word_count.py
@@ -3,13 +3,19 @@ def word_count(sentence):
 
     word_count = {}
 
-    sentence = input('my sentence is:   ')
     words = sentence.split()
    
     for word in words :
         word = word.lower()
         if word in word_count:
             word_count[word] += 1
-        elif word_count[word] == 1:
+        else:
+            word_count[word] = 1
 
-print(word_count)
+    print (word_count)
+s = input('my sentence is:   ')
+word_count(s)
+#     return word_count
+# return is preferred to print as it allow you to utlize or modify it for use in other parts of the code 
+
+# print(word_count())
